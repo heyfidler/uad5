e9a6ee611c670d7f13fe3338f79783c2c347d032
Kerry <heyfidler@gmail.com>
2019-07-11 09:10:28 -0700

    add jokes java module

---
+++ jokes/src/main/java/com/fidflop/jokes/Jokes.java
@@ -0,0 +1,22 @@
+package com.fidflop.jokes;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Jokes {
+    private static List<String> jokes;
+    private static Random random = new Random();
+
+    static {
+        jokes = Arrays.asList(
+            "Q: What goes up and down but does not move?\nA: Stairs",
+            "Q: Where should a 500 pound alien go?\nA: On a diet",
+            "Q: What did one toilet say to the other?\nA: You look a bit flushed. "
+        );
+    }
+
+    public static String getJoke() {
+        return jokes.get(random.nextInt(jokes.size()));
+    }
+}
