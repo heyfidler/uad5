603e1007bd54b9a4e50345c5d80e7f2a91f3bb94
Kerry <heyfidler@gmail.com>
2019-07-20 08:12:03 -0700

    add android library module

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.fidflop.jokes.Jokes;
+import com.fidflop.jokesdisplaymodule.JokeDisplayActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,7 +44,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, Jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, Jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        String joke = Jokes.getJoke();
+        Intent intent =new Intent(this, JokeDisplayActivity.class);
+        intent.putExtra("joke",joke);
+        startActivity(intent);
     }
 
 
---
+++ jokesdisplaymodule/src/main/java/com/fidflop/jokesdisplaymodule/JokeDisplayActivity.java
@@ -0,0 +1,37 @@
+package com.fidflop.jokesdisplaymodule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class JokeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_display);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("joke");
+        TextView jokeTV = findViewById(R.id.joke_tv);
+        jokeTV.setText(joke);
+
+    }
+
+}
